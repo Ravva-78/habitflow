df6e926bb5c76db8170e2dac30fd39691b9f8de9
Smart-MedicationDispenser <majorproject67sem@gmail.com>
2026-07-07 23:22:12 +0530

    Fix Web build and add render.yaml for automatic deployment

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    presets: [['babel-preset-expo', { unstable_transformImportMeta: true }]],
+    plugins: [
+      '@babel/plugin-proposal-export-namespace-from',
+      'react-native-reanimated/plugin',
+    ],
   };
 };
